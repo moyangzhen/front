b0b616d77ea3574e964d075b188141357265d0ac
ludaohua <15711476734@163.com>
2022-11-13 12:07:52 +0800

    添加vuelidate来对表单进行验证

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,22 @@
   <div id="app">
     <div class="layui-container">
       <form class="layui-form layui-form-pane" action="">
-        <div class="layui-form-item">
+        <div class="layui-form-item" :class="{'form-group--error':$v.name.$error}">
           <label class="layui-form-label">用户名</label>
-          <div class="layui-input-block">
-            <input type="text" name="title" v-model="name" required lay-verify="required" placeholder="请输入标题" autocomplete="off" class="layui-input" />
+          <div class="layui-input-inline">
+            <input type="text" name="title" v-model.trim="name" @input="setName($event.target.value)" placeholder="请输入标题" autocomplete="off" class="layui-input" />
           </div>
+          <div class="error layui-form-mid" v-if="!$v.name.required">
+            用户名不得为空</div>
+          <div class="error layui-form-mid" v-if="!$v.name.email">
+            用户名输入格式错误</div>
         </div>
-        <div class="layui-form-item">
+        <div class="layui-form-item" :class="{'form-group--error':$v.password.$error}">
           <label class="layui-form-label">密码</label>
-          <div class="layui-input-block">
-            <input type="password" name="title" v-model="password" required lay-verify="required" placeholder="请输入标题" autocomplete="off" class="layui-input" />
+          <div class="layui-input-inline">
+            <input type="password" name="title" v-model.trim="password" @input="setPassword($event.target.value)" placeholder="请输入密码" autocomplete="off" class="layui-input" />
           </div>
+          <div class="error layui-form-mid" v-if="!$v.password.required">密码不能为空</div>
         </div>
         <div class="layui-form-item">
           <label class="layui-form-label">验证码</label>
@@ -29,6 +34,7 @@
 </template>
 <script>
 import axios from 'axios'
+import { required, email } from 'vuelidate/lib/validators'
 export default {
   name: 'app',
   data () {
@@ -37,7 +43,19 @@ export default {
       name: '',
       password: '',
       code: '',
-      errorMsg: []
+      errorMsg: ''
+    }
+  },
+  validations: {
+    name: {
+      required,
+      email
+    },
+    password: {
+      required
+    },
+    code: {
+      required
     }
   },
   mounted () {
@@ -53,6 +71,14 @@ export default {
           }
         }
       })
+    },
+    setName (value) {
+      this.name = value
+      this.$v.name.$touch()
+    },
+    setPassword (value) {
+      this.password = value
+      this.$v.password.$touch()
     },
     checkForm () {
       this.errorMsg = []
@@ -91,6 +117,14 @@ input {
   margin-left: 10px;
   &:hover {
     color: #009688;
+  }
+}
+.error {
+  display: none;
+}
+.form-group--error {
+  .error {
+    display: block;
   }
 }
 </style>
